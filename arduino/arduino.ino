4e4d00bcb8b671080a10ba850af3b82316c91cfd
violet <violet@violetcraze.net>
2020-11-05 07:05:50 -0700

    saving progress

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,10 +5,13 @@
 #define LIMIT_1 7
 #define LIMIT_2 6
 
-#define PEN_DOWN 60
-#define PEN_UP 0
+#define PEN_DOWN 75
+#define PEN_UP 20
 
 #define LINE_BUFFER_SIZE 100
+
+#define ABSOLUTE_MODE 0
+#define RELATIVE_MODE 1
 
 Adafruit_MotorShield ms = Adafruit_MotorShield();
 
@@ -17,10 +20,22 @@ Adafruit_StepperMotor *step2 = ms.getStepper(200, 2);
 
 Servo penServo;
 
+int coordMode;
+float positionX;
+float positionY;
+
+void writeFloat(float a) {
+  byte bytes[4];
+  memcpy(bytes, (byte*) (&a), 4);
+  Serial.write(bytes, 4);
+}
+
 void goHome() {
   while (digitalRead(LIMIT_1) == HIGH || digitalRead(LIMIT_2) == HIGH) {
     step1->onestep(FORWARD, INTERLEAVE);
   }
+  positionX = 0;
+  positionY = 0;
   step1->release();
 }
 
@@ -33,8 +48,8 @@ void penDown() {
 }
 
 void processCommand(char* command) {
-  Serial.print("Recived: ");
-  Serial.println(command);
+//  Serial.print("Recived: ");
+//  Serial.println(command);
 
   if (strcmp(command, "G28") == 0) {
     goHome();
@@ -46,6 +61,20 @@ void processCommand(char* command) {
 
   if (strcmp(command, "M03") == 0) {
     penDown();
+  }
+
+  if (strcmp(command, "G90") == 0) {
+    coordMode = ABSOLUTE_MODE;
+  }
+
+  if (strcmp(command, "G91") == 0) {
+    coordMode = RELATIVE_MODE;
+  }
+
+  if (strcmp(command, "M114") == 0) {
+    Serial.print("RM114");
+    writeFloat(positionX);
+    writeFloat(positionY);
   }
 }
 
@@ -71,6 +100,10 @@ void setup() {
   pinMode(LIMIT_1, INPUT);
 
   penServo.attach(10);
+  
+  coordMode = ABSOLUTE_MODE;
+  positionX = -1;
+  positionY = -1;
 
   ms.begin();
 }
